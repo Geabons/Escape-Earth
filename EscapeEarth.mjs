bd921ad3191266edee0f2185ce29b56e85e23529
Geabons <Geabonzo@gmail.com>
2025-03-09 22:21:25 +0100

    task 1 done

--- EscapeEarth.mjs
+++ EscapeEarth.mjs
@@ -1,21 +1,33 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 const SOLAR_API = "https://api.le-systeme-solaire.net/rest/";
 const playerId = "Martinlen@uia.no";
 const GAME_API = "https://spacescavanger.onrender.com/";
 
-(async function() {
-    try {
-      const startUrl = `${GAME_API}start?player=${encodeURIComponent(playerId)}`;
-      const startResponse = await fetch(startUrl);
-      const startData = await startResponse.json();
-      console.log("Challenge started:", startData);
+(async function () {
+  try {
+    const startUrl = `${GAME_API}start?player=${encodeURIComponent(playerId)}`;
+    const startResponse = await fetch(startUrl);
+    const startData = await startResponse.json();
+    console.log("Challenge started:", startData);
 
-      const sunResponse = await fetch(`${SOLAR_API}bodies/soleil`);
-      const sunData = await sunResponse.json();
-      console.log("sun data:", sunData);
+    const sunResponse = await fetch(`${SOLAR_API}bodies/sun`);
+    const sunData = await sunResponse.json();
+    //console.log("sun data:", sunData);
 
-    } catch (error) {
-        console.error("Error:", error);
-      }
+    const sunEquaRadius = sunData.equaRadius;
+    const sunMeanRadius = sunData.meanRadius;
+    const accessPin = sunEquaRadius - sunMeanRadius;
+    
+    const answerResponse1 = await fetch(`${GAME_API}answer`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ answer: accessPin, player: playerId }),
+    });
+    const answerData1 = await answerResponse1.json();
+    console.log("Answer response:", answerData1);
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
 })();
